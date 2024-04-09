f0e1db2e2e0a70322548f69d62d26989748a9573
Nicolás Miñán <nm.imagenysonido@gmail.com>
2024-04-09 13:52:16 -0300

    add responsive improvemetns

---
+++ _next/static/30Q0I7seouInY6XH_lobm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/117-f5828d1ccab98660.js","static/css/1f8d5571e603426f.css","static/chunks/pages/index-12a8a1b29d305b1c.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
