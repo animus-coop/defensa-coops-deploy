e352d546e2b9cccb177772a02bb44d5ea7c9821c
Nicolás Miñán <nm.imagenysonido@gmail.com>
2024-04-09 13:08:58 -0300

    add factic sign

---
+++ _next/static/UUv09x0wkpX8psMCjgnDw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/117-f5828d1ccab98660.js","static/css/9264631dd416c386.css","static/chunks/pages/index-3608a9967f118240.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
